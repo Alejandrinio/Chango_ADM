998094d7cc1abaa051a15f2a063394fc6f775e91
Thiago <tfernandez@americavirtualsa.com>
2025-08-09 19:53:20 -0300

    sidebar-fino

--- backend/database/chango_adm_fantasia.sql
+++ backend/database/chango_adm_fantasia.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE IF NOT EXISTS `chango_adm_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `chango_adm_db`;
+
 -- MySQL dump 10.13  Distrib 8.4.6, for Win64 (x86_64)
 --
 -- Host: localhost    Database: chango_adm_db
